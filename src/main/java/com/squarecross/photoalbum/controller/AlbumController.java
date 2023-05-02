e6759e8f363225ffed6ac92f76f0df5c88053eca
sghwan <anan123456@naver.com>
2023-05-02 19:27:37 +0900

    Implement AlbumController

---
+++ src/main/java/com/squarecross/photoalbum/controller/AlbumController.java
@@ -0,0 +1,26 @@
+package com.squarecross.photoalbum.controller;
+
+import com.squarecross.photoalbum.dto.AlbumDto;
+import com.squarecross.photoalbum.service.AlbumService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/albums")
+public class AlbumController {
+
+    private final AlbumService albumService;
+
+    @Autowired
+    public AlbumController(AlbumService albumService) {
+        this.albumService = albumService;
+    }
+
+    @GetMapping("/{albumId}")
+    public AlbumDto findAlbumById(@PathVariable Long albumId) {
+        return albumService.getAlbum(albumId);
+    }
+}
