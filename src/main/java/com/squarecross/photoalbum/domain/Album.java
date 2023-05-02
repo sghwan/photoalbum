85c51edc22995f761a7b61b4b4ff26803a20d770
sghwan <anan123456@naver.com>
2023-05-02 15:54:45 +0900

    Implement Photo Domain Class

--- src/main/java/com/squarecross/photoalbum/domain/Album.java
+++ src/main/java/com/squarecross/photoalbum/domain/Album.java
@@ -3,7 +3,9 @@ package com.squarecross.photoalbum.domain;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "album", schema = "photo_album", uniqueConstraints = {@UniqueConstraint(columnNames = "album_id")})
@@ -24,6 +26,9 @@ public class Album {
     @CreationTimestamp
     @Column(name = "created_at", unique = false, nullable = true)
     private Date createdAt;
+
+    @OneToMany(mappedBy = "album", cascade = CascadeType.ALL)
+    private List<Photo> photos = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -47,5 +52,13 @@ public class Album {
 
     public void setCreatedAt(Date createdAt) {
         this.createdAt = createdAt;
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<Photo> photos) {
+        this.photos = photos;
     }
 }
---
+++ src/main/java/com/squarecross/photoalbum/domain/Photo.java
@@ -0,0 +1,97 @@
+package com.squarecross.photoalbum.domain;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "photo", schema = "photo_album", uniqueConstraints = {@UniqueConstraint(columnNames = "photo_id")})
+public class Photo {
+
+    protected Photo() {
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "photo_id", unique = true, nullable = false)
+    private Long id;
+
+    @Column(name = "file_name", unique = false, nullable = true)
+    private String fileName;
+
+    @Column(name = "original_url", unique = false, nullable = true)
+    private String originalUrl;
+
+    @Column(name = "thumb_url", unique = false, nullable = true)
+    private String thumbUrl;
+
+    @Column(name = "file_size", unique = false, nullable = true)
+    private int fileSize;
+
+    @CreationTimestamp
+    @Column(name = "uploaded_at", unique = false, nullable = true)
+    private Date uploadedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "album_id")
+    private Album album;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getThumbUrl() {
+        return thumbUrl;
+    }
+
+    public void setThumbUrl(String thumbUrl) {
+        this.thumbUrl = thumbUrl;
+    }
+
+    public int getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(int fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public Date getUploadedAt() {
+        return uploadedAt;
+    }
+
+    public void setUploadedAt(Date uploadedAt) {
+        this.uploadedAt = uploadedAt;
+    }
+
+    public Album getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(Album album) {
+        this.album = album;
+    }
+}
