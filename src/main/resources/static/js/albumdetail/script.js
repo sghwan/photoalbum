5659335bd383f98aa03520c2bce46268b6565164
sghwan <anan123456@naver.com>
2023-05-20 01:34:08 +0900

    implement albumDetailPage photos move function

--- src/main/resources/static/js/albumdetail/script.js
+++ src/main/resources/static/js/albumdetail/script.js
@@ -48,3 +48,38 @@ function submitForm(method, albumId, control) {
     $form.setAttribute("action", path);
     $form.submit();
 };
+
+function moveSubmit(fromAlbumId, toAlbumId) {
+    if(fromAlbumId == toAlbumId) {
+        alert("같은 앨범으로 이동할 수 없습니다.");
+        return;
+    }
+
+    const photoIds = getCheckedPhotoIds();
+
+    if(photoIds.length == 0) {
+        alert("이동할 파일을 선택해 주세요");
+        return;
+    }
+
+    const param = {fromAlbumId, toAlbumId, photoIds};
+
+    $.ajax({
+        url : "/albums/" + fromAlbumId + "/photos/move",
+        type : "POST",
+        contentType : "application/json",
+        data : JSON.stringify(param),
+        success : function(data) {
+            window.location.href="/albums/" + fromAlbumId;
+        }
+    });
+}
+
+function getCheckedPhotoIds() {
+    let photoIds = [];
+    for(let i = 0; i < $cbArr.length; i++) {
+        if($cbArr[i].checked) photoIds.push($cbArr[i].value);
+    }
+
+    return photoIds;
+}
