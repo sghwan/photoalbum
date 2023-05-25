ec9db9e20d9c486dde62615cb9f2345759fa6749
sghwan <anan123456@naver.com>
2023-05-25 21:39:46 +0900

    refactoring photoUploadPage(removing waiting file function)

--- src/main/resources/static/js/photoupload/script.js
+++ src/main/resources/static/js/photoupload/script.js
@@ -1,7 +1,7 @@
 const $dropZone = document.getElementById("drop_zone");
 const $p = document.querySelector("p");
 const $fileUpload = document.getElementById('file_upload');
-const arr = [];
+const fileArr = [];
 
 function changeHandler(ev) {
     if (ev.target.files) {
@@ -30,7 +30,8 @@ function addPreviewImage(ev, arr) {
         const reader = new FileReader();
         const $img = document.createElement("img");
         const $box = document.createElement("div");
-        const $close = document.createElement("img");
+        const $remove = document.createElement("div");
+        const $x = document.createElement("img");
         reader.readAsDataURL(file);
         reader.onloadend = function () {
             ev.preventDefault()
@@ -42,15 +43,34 @@ function addPreviewImage(ev, arr) {
             $box.classList.add("preview-box");
             $box.appendChild($img);
 
-            $close.classList.add("preview-close-btn");
-            $box.appendChild($close);
+            $x.src = "/assets/x.png";
+            $x.classList.add("x-i")
+            $remove.classList.add("preview-close-btn");
+            $remove.appendChild($x);
+            $remove.onclick = function() {removePreview(ev, file, i)};
+            $remove.id = i;
+            $box.appendChild($remove);
         }
         $dropZone.appendChild($box);
-        arr.push(file);
+        fileArr.push(file);
     });
 
+    setFiles();
+}
+
+function removePreview(ev, f, i) {
+    const idx = fileArr.indexOf(f);
+    if(idx > -1) {
+        fileArr.splice(idx, 1);
+        const $tmp = document.getElementById(i);
+        $dropZone.removeChild($tmp.parentNode);
+        setFiles();
+    }
+}
+
+function setFiles() {
     const dt = new DataTransfer();
-    for (let f of arr) {
+    for (let f of fileArr) {
         dt.items.add(f);
     }
     $fileUpload.style.display = "none";
