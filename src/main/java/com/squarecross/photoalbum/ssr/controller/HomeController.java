cf2c9b58fe0b68ac81c292990925666b1606f01f
sghwan <anan123456@naver.com>
2023-06-08 18:38:13 +0900

    implement homeController redirect to /albums uri

---
+++ src/main/java/com/squarecross/photoalbum/ssr/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.squarecross.photoalbum.ssr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homeRedirection() {
+        return "redirect:/albums";
+    }
+}
